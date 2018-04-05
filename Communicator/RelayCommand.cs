2f944f8495327e90e52b27f446ebd91d0eba50ad
wojciechmizera <34011256+wojciechmizera@users.noreply.github.com>
2018-04-05 13:17:00 +0200

    v 0.1 General visual appearance Created button bindings

---
+++ Communicator/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Communicator
+{
+    class RelayCommand : ICommand
+    {
+
+        private Action<object> action;
+
+        private Func<bool> func;
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs());
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (func != null)
+                return func();
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public RelayCommand(Action<object> action, Func<bool> func)
+        {
+            this.action = action;
+            this.func = func;
+        }
+    }
+}
